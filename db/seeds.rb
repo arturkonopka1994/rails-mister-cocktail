dcfc8ee67a2ef15cec73cb987d44bbec3fc816e2
Artur Konopka <artur.konopka.1994@gmail.com>
2020-07-21 20:54:56 +0100

    Created Api pulling out all the ingredients from the JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+class IngredientApi
+  
+  def initialize
+    @url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+    retrieve_data
+  end
+
+  def retrieve_data
+    opened = open(@url).read
+    @data = JSON.parse(opened)
+  end
+
+  def extract_lines
+    drinks = @data["drinks"]
+    drinks.map do |line|
+      newHash = {
+        name: line["strIngredient1"]
+      }
+    end
+  end
+end
+
+Ingredient.create(IngredientApi.new.extract_lines)
