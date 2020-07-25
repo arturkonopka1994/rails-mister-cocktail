1854f790e0642c75e01307bf4bf5d11016eb26bb
Artur Konopka <artur.konopka.1994@gmail.com>
2020-07-25 10:55:17 +0100

    fixed errors, added styling

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,7 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @dose = Dose.new
   end
 
   def new
@@ -12,7 +13,7 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.new(strong_params)
+    @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -35,7 +36,7 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
   end
 
-  def strong_params
+  def cocktail_params
     params.require(:cocktail).permit(:name)
   end
 end
